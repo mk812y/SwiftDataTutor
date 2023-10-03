f5c76b004a558364b9b4cc82f08f774e6a9aecc4
mk812y <mk812y@gmail.com>
2023-10-03 18:13:42 +0200

    Initial Commit

---
+++ SwiftDataTutor/SwiftDataTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorApp.swift
+//  SwiftDataTutor
+//
+//  Created by Михаил Куприянов on 3.10.23..
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
